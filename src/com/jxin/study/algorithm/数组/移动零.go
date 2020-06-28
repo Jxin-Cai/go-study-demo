3fb90cd30d9f35023918a5b6d12f8d9e0377818e
jxin <105760730@qq.com>
2020-06-28 19:28:50 +0800

    完成移动0

---
+++ src/com/jxin/study/algorithm/数组/移动零.go
@@ -0,0 +1,57 @@
+package main
+
+func main() {
+
+}
+func moveZeroes1(nums []int) {
+	if nums == nil {
+		return
+	}
+	j := 0
+	for i := 0; i < len(nums); i++ {
+		if nums[i] == 0 {
+			continue
+		}
+
+		if i == j {
+			j++
+			continue
+		}
+		nums[i], nums[j] = nums[j], nums[i]
+		j++
+	}
+}
+func moveZeroes2(nums []int) {
+	if nums == nil {
+		return
+	}
+	i := 0
+	for i < len(nums) && nums[i] != 0 {
+		i++
+	}
+	j := i
+	for ; i < len(nums); i++ {
+		if nums[i] == 0 {
+			continue
+		}
+		nums[i], nums[j] = nums[j], nums[i]
+		j++
+	}
+}
+func moveZeroes3(nums []int) {
+	// 递减非零计数器
+	l := len(nums)
+	// 递增非零计数器
+	i := 0
+	for i < l {
+		if nums[i] != 0 {
+			i++
+			continue
+		}
+		l--
+		// 把0前后两部分合并
+		nums = append(nums[0:i], nums[i+1:]...)
+		// 在末尾补回0
+		nums = append(nums, 0)
+	}
+}
